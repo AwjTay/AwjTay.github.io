98be058391e10b6c48699bb94ea308ae2e7e910b
andy <andy@olivers-air.home>
2019-04-07 21:12:33 +0100

    Commit of built app for hosting

---
+++ precache-manifest.84c28c755cf16f4b86b2a508f85eda26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "93bfa29b35852434ba1f",
+    "url": "/static/css/main.691b32fa.chunk.css"
+  },
+  {
+    "revision": "93bfa29b35852434ba1f",
+    "url": "/static/js/main.7c3c2f73.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "87b3c7f05cabcb0813c1",
+    "url": "/static/js/2.9cd9e6b4.chunk.js"
+  },
+  {
+    "revision": "0f11ff6321a075e1bee7742a84a39060",
+    "url": "/index.html"
+  }
+];
